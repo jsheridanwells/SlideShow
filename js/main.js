b7e4872a3439ba19d451a68e1542bf0c64eaf4f5
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-08 09:12:08 -0500

    more jquery work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-// 'use strict';
+'use strict';
 
-// var Slides = (function(){
-	
-// }());
+var Slides = (function() {
+	$(document).ready(function () {
+		$('.fa-arrow-left').click(function(){
+			Slides.getPreviousImage();
+		});
+		$('.fa-arrow-right').click(function(){
+			Slides.getNextImage();
+		});
+	});
+	return {
+		getPreviousImage: function () {
+			let currentId = $('#image-frame').id;
+		},
+		getNextImage: function () {
+
+		}
+	};
+} ());
